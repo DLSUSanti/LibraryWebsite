b69c25b259dc3ce63ea40cbc8949987af7cf1d0d
DLSUSanti <santiago_alvarez@dlsu.edu.ph>
2019-10-23 22:52:02 +0800

    preparing search queries

--- server.js
+++ server.js
@@ -66,8 +66,15 @@ app.post("/10th-Floor-Mezzanine", (req,res)=>{
     })
 })
 
-app.post("/SearchShelf", (req,res)=>{
-
+app.post("/SearchShelf", urlencoder, (req,res)=>{
+    console.log("searching")
+    var query = req.body.basicquery
+    var exp = new RegExp(query, 'i')
+    console.log(exp)
+    
+    Shelve.find({$or:[{subject_names: exp}, {subject_codes: exp}, {location: exp}, {section: exp}]}).exec(function(err,results){
+        console.log(results)
+    })
 })
 
 
