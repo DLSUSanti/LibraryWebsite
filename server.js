ce6f5c5a2940ff0ae2afbb9e7e99813665a6723d
Kyle Ty <31773530+Jade03022@users.noreply.github.com>
2019-11-06 21:08:06 +0800

    Added login and admin views. Password generator also implemented.
    Added very simple UIs for the login and admin views. The password generator was also implemented. As of now, it is set to change every 1 minute for convenience of testing.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const session = require("express-session")
 const bodyparser = require("body-parser")
+const cookieparser = require("cookie-parser")
 const hbs = require("hbs")
 const url = require("url")
+const generator = require("generate-password");
 
 const mongoose = require("mongoose")
 const connectionString = "mongodb+srv://admin:admin@librarylocator-nqyws.gcp.mongodb.net/librarylocator?retryWrites=true&w=majority"
@@ -14,11 +16,43 @@ const urlencoder = bodyparser.urlencoded({
 
 mongoose.Promise = global.Promise
 mongoose.connect(connectionString, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
 
 app.use(express.static(__dirname + "/public"))
 const Shelve = require("./models/database.js").Shelve
+const User = require("./models/database.js").User
+
+app.use(cookieparser())
+
+app.get("/", (req, res)=>{
+    if(!req.cookies["password_change"]){
+        res.cookie("password_change", "OneMinute", {
+            maxAge : 60000 // 60000 milliseconds = 1 minute.
+        })
+        
+        var password = generator.generate({
+            length: 6,
+            numbers: true
+        });
+        
+        User.updateOne({
+            username: "guest"
+        }, {
+            password: password
+        }, (err, doc)=>{
+            if(err){
+                res.send(err)
+            }
+        })
+    }
+    
+    res.render("login.hbs")
+})
+
+/*
+Commented this out because the application now starts at the login.hbs instead of home.hbs
 
 app.get("/", (req,res)=>{
      Shelve.find({location:"7th floor Mezzanine"}).exec(function(err,shelves){
@@ -27,6 +61,82 @@ app.get("/", (req,res)=>{
             location: "7th Floor Mezzanine",
             data: shelves
         })
+    })
+})
+*/
+
+app.post("/login", urlencoder, (req, res)=>{
+    let username = req.body.username;
+    let password = req.body.password;
+    
+    User.findOne({
+        username: username
+    }, (err, doc)=>{
+        if(err)
+           res.send(err)
+        else if(!doc){
+            res.render("login.hbs", {
+                invalid_credentials: "Invalid username or password!"
+            })
+        }
+        else{
+            var passwordFromDB = doc.password
+                        
+            if(password != passwordFromDB){
+                res.render("login.hbs", {
+                    invalid_credentials: "Invalid username or password!"
+                })
+            }
+            else{
+               if(username == "admin"){
+                   res.redirect(307, "/admin")
+               }
+                
+               else{
+                   res.redirect(307, "/7th-Floor-Mezzanine")
+               }  
+            }    
+        }    
+    })
+})
+
+app.get("/logout", (req, res)=>{
+    if(!req.cookies["password_change"]){
+        res.cookie("password_change", "OneMinute", {
+            maxAge : 60000 // 60000 milliseconds = 1 minute.
+        })
+        
+        var password = generator.generate({
+            length: 6,
+            numbers: true
+        });
+        
+        User.updateOne({
+            username: "guest"
+        }, {
+            password: password
+        }, (err, doc)=>{
+            if(err){
+                res.send(err)
+            }
+        })
+    }
+    
+    res.render("login.hbs")
+})
+
+app.post("/admin", (req, res)=>{
+    User.findOne({
+        username: "guest"
+    }, (err,doc)=>{
+        if(err){
+            res.send(err)
+        }
+        else{
+            res.render("admin.hbs", {
+                password: doc.password
+            })
+        }
     })
 })
 
